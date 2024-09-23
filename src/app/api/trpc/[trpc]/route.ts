cc6ffda325af20f5b661eca84bd17473318b596d
DawidRubch <dawid.niegrebecki@gmail.com>
2024-09-23 10:32:10 +0200

    Fixed TRPC

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,13 +1,46 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 import { appRouter } from "@/server/routers";
+import { type NextRequest } from "next/server";
+import { getAuth } from "@clerk/nextjs/server";
+import { env } from "process";
 
-const handler = (req: Request) =>
+type AuthObject = ReturnType<typeof getAuth>;
+
+
+export const createTRPCContext = async (opts: {
+    headers: Headers;
+    auth: AuthObject;
+}) => {
+    return {
+        userId: opts.auth.userId,
+        ...opts,
+    };
+};
+
+
+const createContext = async (req: NextRequest) => {
+    return createTRPCContext({
+        headers: req.headers,
+        auth: getAuth(req),
+    });
+};
+
+
+const handler = (req: NextRequest) =>
     fetchRequestHandler({
         endpoint: "/api/trpc",
         req,
         router: appRouter,
-        createContext: () => ({}),
+        createContext: () => createContext(req),
+        onError:
+            env.NODE_ENV === "development"
+                ? ({ path, error }) => {
+                    console.error(
+                        `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+                    );
+                }
+                : undefined,
     });
 
 export { handler as GET, handler as POST };
