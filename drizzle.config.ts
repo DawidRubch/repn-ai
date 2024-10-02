37a0a4513ad6094ce61c0984bf1f6fee2da267d3
DawidRubch <dawid.niegrebecki@gmail.com>
2024-10-02 09:35:27 +0200

    Setup drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,19 @@
 
 import { defineConfig } from 'drizzle-kit'
+import { config } from 'dotenv';
+
+config({ path: '.env.local' });
+
+
 export default defineConfig({
     schema: "./src/db/schema.ts",
     dialect: 'postgresql',
+
     migrations: {
         prefix: 'supabase'
+    },
+    dbCredentials: {
+        url: process.env.DB_URL as string
     }
+
 })
